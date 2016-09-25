78c8ed2d81886fa6626173daeba63e6319ffff81
Viktor Trako <viktor.trako@gmail.com>
2016-09-25 22:51:54 +0100

    update

--- deploy_app.sh
+++ deploy_app.sh
@@ -3,9 +3,11 @@ echo "*** deploying project ***"
 
 #  write out jar file versions and names to VERSIONS.txt file and save the
 #  application name to an env var
-app_name=`ls -1 target/*.jar  | cut -d "/" -f 2 | tee VERSIONS.txt | grep -v original | tail -n 1 | cut -d "-" -f 1`
+app_name=`ls -1 target/*.war  | cut -d "/" -f 2 | tee VERSIONS.txt | grep -v original | tail -n 1 | cut -d "-" -f 1`
 
+/opt/google-cloud-sdk/bin/gcloud --quiet components update
 /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file account.json
+/opt/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
 
 gsutil cp -r target/appengine-try-java-1.0/* gs://${GSTORAGE_DEST_BUCKET}/appengine-try-java-1.0/
 
@@ -17,7 +19,7 @@ fi
 
 echo "Deploying to AppEngine"
 set -x
-cd $HOME/$CIRCLE_PROJECT_REPONAME && gcloud -q app deploy app.yaml --promote --version=staging
+cd $HOME/$CIRCLE_PROJECT_REPONAME && gcloud -q app deploy app.yaml
 set +x
 
 if [ $? -ne 0 ]; then
