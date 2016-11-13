abba45fef95c879b13607a229c6e5e1f52c7aa8b
Viktor Trako <viktor.trako@gmail.com>
2016-11-13 15:35:29 +0000

    use sdk directly to update

--- deploy_app.sh
+++ deploy_app.sh
@@ -27,7 +27,7 @@ if [ $ret -ne 0 ]; then
   exit $ret
 fi
 
-curl -o appengine-java-sdk-1.9.46.zip https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.46.zip
+curl -o $HOME/appengine-java-sdk-1.9.46.zip https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.46.zip
 unzip -q -d $HOME $HOME/appengine-java-sdk-1.9.46.zip
 
 cd $HOME/$CIRCLE_PROJECT_REPONAME && $HOME/appengine-java-sdk-1.9.46/bin/appcfg.sh -A $GCLOUD_PROJECT -V 2 update
