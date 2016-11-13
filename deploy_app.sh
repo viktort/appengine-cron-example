de174d711c4a8e9db13477a6dd7dd78b9aeadd2d
Viktor Trako <viktor.trako@gmail.com>
2016-11-13 14:19:57 +0000

    fix auto deploy

--- deploy_app.sh
+++ deploy_app.sh
@@ -8,20 +8,19 @@ app_name=`ls -1 target/*.war  | cut -d "/" -f 2 | tee VERSIONS.txt | grep -v ori
 gcloud components update app
 gcloud auth activate-service-account --key-file account.json
 
-gsutil cp -r target/appengine-try-java-1.0/* gs://${GSTORAGE_DEST_BUCKET}/appengine-try-java-1.0/
+gcloud config set project $GCLOUD_PROJECT
 
-ret=$?
-if [ $ret -ne 0 ]; then
-  echo "Failed to cp application files to gstorage"
-  exit $ret
-fi
-
-echo "Deploying to AppEngine"
-set -x
-cd $HOME/$CIRCLE_PROJECT_REPONAME && /opt/google-cloud-sdk/bin/gcloud -q app deploy app.yaml
-set +x
+gsutil ls -L gs://"${GSTORAGE_DEST_BUCKET}"
 
 if [ $? -ne 0 ]; then
-  echo "Failed to deploy to AppEngine"
-  exit $?
+
+    echo gs://"${GSTORAGE_DEST_BUCKET}" bucket does not exist
+    gsutil mb -l europe-west1 -p $GOOGLE_PROJECT_ID gs://"${GSTORAGE_DEST_BUCKET}"
+    echo $?
+
 fi
+
+curl -o $HOME/google_appengine_1.9.40.zip https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.40.zip
+unzip -q -d $HOME $HOME/google_appengine_1.9.40.zip
+
+cd $HOME/$CIRCLE_PROJECT_REPONAME && gcloud --quiet preview app deploy app.yaml --version 1 --promote
