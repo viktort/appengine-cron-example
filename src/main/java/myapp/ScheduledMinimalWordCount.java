57533d32f008304d39ab9504e4eece450d773bde
Viktor Trako <viktor.trako@gmail.com>
2016-11-14 16:40:22 +0000

    memory

--- src/main/java/myapp/ScheduledMinimalWordCount.java
+++ src/main/java/myapp/ScheduledMinimalWordCount.java
@@ -26,7 +26,7 @@ public class ScheduledMinimalWordCount {
     Pipeline p = Pipeline.create(options);
 
     System.out.println("get here 0");
-    p.apply(TextIO.Read.from("gs://dataflow-samples/shakespeare/allswellthatendswell.txt"))
+    p.apply(TextIO.Read.from("gs://dataflow-samples/shakespeare/loverscomplaint.txt"))
         .apply(ParDo.named("ExtractWords").of(new DoFn<String, String>() {
           @Override
           public void processElement(ProcessContext c) {
